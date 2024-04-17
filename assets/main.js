3acee03268aaf79040e28f7b0bc89d8b6e7a1250
binh <dbcl@dkpharm.vn>
2024-04-17 09:41:48 +0700

    17042024

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,66 @@
-var vitri = document.querySelector('.vi_tri')
-var ngay = document.querySelector('.ngay')
-var dodan = document.querySelector('.dodan')
-var nhietdo = document.querySelector('.nhietdo')
-var visinh = document.querySelector('.visinh')
-var ph = document.querySelector('.ph')
-var submit = document.querySelector('.submit')
+const ngay = document.querySelector('.ngay')
+const vitri = document.querySelector('.vi_tri')
+const dodan = document.querySelector('.dodan')
+const nhietdo = document.querySelector('.nhietdo')
+const visinh = document.querySelector('.visinh')
+const ph = document.querySelector('.ph')
+const can = document.querySelector('.can')
+const submit = document.querySelector('.submit')
 
+const block__visinh = document.querySelector('.block__vi-sinh')
+const block__ph = document.querySelector('.block__ph')
+const block__can = document.querySelector('.block__can')
+
+
+vitri.addEventListener('keydown', (event)=>{
+setTimeout(() => {
+    showandhide(event) 
+}, 0);
+} )
+
+
+function showandhide(event) {
+    var value = event.target.value
+    console.log(value)
+    if(value == 1 || value == 2){
+       block__visinh.classList.remove('show')
+       block__visinh.classList.add('hide')
+   
+       block__can.classList.remove('hide')
+       block__can.classList.add('show')
+       
+       block__ph.classList.remove('hide')
+       block__ph.classList.add('show')
+   
+   } else if(value == 3 || value == 4) {
+       block__visinh.classList.remove('show')
+       block__visinh.classList.add('hide')
+   
+       block__can.classList.remove('show')
+       block__can.classList.add('hide')
+       
+       block__ph.classList.remove('show')
+       block__ph.classList.add('hide')
+   
+   } else{
+   
+       block__visinh.classList.remove('hide')
+       block__visinh.classList.add('show')
+   
+       block__can.classList.remove('show')
+       block__can.classList.add('hide')
+       
+       block__ph.classList.remove('show')
+       block__ph.classList.add('hide')
+   
+   }
+}
 
 
 
 submit.addEventListener('click', ()=>{
     
-    var data = {
+    const data = {
         ngay : 'ngay',
         vitri: 'Vị trí',
         dodan: 'Độ dẫn',
@@ -21,9 +70,9 @@ submit.addEventListener('click', ()=>{
         can: 'can'
     }    
     
-    var url = "https://script.google.com/macros/s/AKfycbzsME5ExqhKEUuKWr5gX62Cs6IQpPr7bGKCNK97cyXHpdBSSsAPQPfGdoxQTWKvXuVRCw/exec?data="
+    const url = "https://script.google.com/macros/s/AKfycbzsME5ExqhKEUuKWr5gX62Cs6IQpPr7bGKCNK97cyXHpdBSSsAPQPfGdoxQTWKvXuVRCw/exec?data="
       data = JSON.stringify(data)
-    var fullUrl = url+data
+    const fullUrl = url+data
     fetch(fullUrl)
     .then((res)=>{
         console.log(res)
@@ -33,30 +82,6 @@ submit.addEventListener('click', ()=>{
 
 
 
-
-
-
-
-
-vitri.addEventListener('change', (e)=>{
- var value = e.target.value
- 
-
- 
- 
- var nguon = ['W1' , 'W2' ,'W3' , 'W4', 'W1.1' , 'W2.1' ,'W3.1' , 'W4.1']
- if(nguon.includes(value)){
-    viTriNguon.forEach(vitri=>{
-        vitri.classList.remove('hide')
-    })
- } else {
-    if(nguon.includes(value)){
-        viTriNguon.forEach(vitri=>{
-            vitri.classList.add('hide')
-        })
-
- }
-}})
 
 
 
